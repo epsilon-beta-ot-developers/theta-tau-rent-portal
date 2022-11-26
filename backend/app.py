62eb3d717cc126e426d95f47ca1ba297f6744282
ForeverASilver <77247305+ForeverASilver@users.noreply.github.com>
2022-11-26 00:31:12 -0500

    Restructure layout and add backend files

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from pymongo import MongoClient
+import json
+
+app = Flask(__name__, static_url_path='', static_folder="../")
+
+@app.route("/")
+def index_redir():
+    return app.send_static_file("index.html")
+
+app.run(host="0.0.0.0", debug=True)

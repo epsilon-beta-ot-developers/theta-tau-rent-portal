e4b78c6b0644cdde22215e6b28d4c3ed12279020
Nick Bely <67796036+nbely@users.noreply.github.com>
2024-02-25 17:38:23 -0500

    [Webapp] Change logout to Forbidden navigation if no valid user id token

--- src/components/PrivateRoutes/PrivateRoutes.tsx
+++ src/components/PrivateRoutes/PrivateRoutes.tsx
@@ -1,20 +1,19 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Forbidden from "@/pages/ErrorPages/Forbidden";
-import { logout, type RootState } from "@/store";
+import { type RootState } from "@/store";
 
 export interface PrivateRoutesProps {
   roles?: string[];
 }
 
 const PrivateRoute: FC<PrivateRoutesProps> = ({ roles }) => {
-  const dispatch = useDispatch();
   const { user } = useSelector((state: RootState) => state.auth);
 
   if (!user) {
-    dispatch(logout());
+    return <Forbidden />;
   } else if (roles && !roles.includes(user.role)) {
     return <Forbidden />;
   } else {
